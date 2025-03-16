66af5a953355a79ef89078c3ce0ae363b4c5c3e9
Mugundhan-strugglebreaker <srmvmugund@gmail.com>
2025-03-16 19:22:45 +0530

    creating spring boot project

---
+++ src/main/java/com/learn_spring_batch/learn_spring_batch/LearnSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.learn_spring_batch.learn_spring_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringBatchApplication.class, args);
+	}
+
+}
